579a12b0dd10d9c218062a19f0ae457b5b8825f8
Naisie <anais.ratanavanh@gmail.com>
2018-11-19 20:04:11 +0100

    Fichier de lancement d'airmon-ng et airodump-ng

---
+++ launch_airodump.sh
@@ -0,0 +1,25 @@
+#!bin/bash
+#Script qui permet de lancer airodump-ng 
+#Enregistre les sorties sur un fichier à part dans le répertoire /tmp (VOIR COMMENT TRANSFORMER EN BDD + AMELIORER LE SCRIPT)
+#Mettre en $1 [start/stop/check]
+#Mettre en $2 l'interface de votre machine (voir commande airmon-ng)
+
+string_date=$(date +%F%H%M%S)
+string_mon='mon'
+
+if [ "$1" == "start" ]
+then
+	airmon-ng start "$2"
+	airodump-ng -w /tmp/"$string_date" "$2$string_mon" 
+	echo "$?"
+elif [ "$1" == "stop" ]
+then 
+	airmon-ng stop "$2$string_mon"
+	echo "$?"
+elif [ "$1" == "check" ]
+then
+	echo "ok"
+	echo "$?"
+else
+	echo "Bad argument"
+fi
