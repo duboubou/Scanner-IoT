3741c1723c43c7f14a4725c325b34e0aca2d3c67
duboubou <44380490+duboubou@users.noreply.github.com>
2018-12-18 12:56:27 +0100

    Add files via upload

--- rtl-php-sh.php
+++ rtl-php-sh.php
@@ -22,13 +22,43 @@
 <?php
 if(!empty($_GET["start"])){
   $start=shell_exec('/var/www/html/script_rtl.sh start 2>&1');
-//  print_r($start);
-//  echo "<pre>$start</pre>";
-
-  //sleep(50);
-
   $cut=shell_exec('/var/www/html/cut.sh');
   $db=shell_exec('/var/www/html/load.sh');
+
+  sleep(5);
+
+  $con=mysqli_connect("localhost","bryan","podium123","projet");
+  if(mysqli_connect_errno())
+	{
+	  echo "Failed" . mysqli_connect_errno();
+	}
+
+  $result=mysqli_query($con,"SELECT * FROM rtl");
+
+  echo "<table border='1'>
+  <tr>
+  <th>Timestamp</th>
+  <th>Device</th>
+  <th>Temperature</th>
+  <th>Humidity</th>
+  <th>Rain</th>
+  <th>Pressure</th>
+  </tr>";
+
+  while($row = mysqli_fetch_array($result))
+	{
+	  echo "<tr>";
+	  echo "<td>" . $row['timestamp'] . "</td>";
+	  echo "<td>" . $row['device'] . "</td>";
+	  echo "<td>" . $row['temperature'] . "</td>";
+	  echo "<td>" . $row['humidity'] . "</td>";
+	  echo "<td>" . $row['rain'] . "</td>";
+	  echo "<td>" . $row['pressure'] . "</td>";
+	  echo "</tr>";
+	}
+	echo "</table>";
+
+  mysqli_close($con);
 
 }elseif (!empty($_GET["stop"])) {
   $stop=shell_exec('/var/www/html/script_rtl.sh stop');
