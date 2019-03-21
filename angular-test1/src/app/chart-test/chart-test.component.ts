b7f65d98d95c9ff58172baf8f31f20f2fce9061b
solennn <45072303+solennn@users.noreply.github.com>
2019-03-21 09:39:18 +0100

    Add files via upload

--- angular-test1/src/app/chart-test/chart-test.component.ts
+++ angular-test1/src/app/chart-test/chart-test.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
 import { Observable } from 'rxjs';
 import { subscribeOn } from 'rxjs/operators';
 import { ReturnsJsonArrayService } from 'src/app/returns-json-array.service';
+
 declare const Chart;
 
 @Component({
@@ -17,7 +18,7 @@ export class ChartTestComponent implements OnInit {
   content_cam_rtl = 0;
   content_tab_rtl: any;
   content_tab_ble: any;
-  content_tab_wifi: any;
+  content_tab_wifi2: any;
   content_tab_stations: any;
   isInit = false;
   ip = '172.20.10.2';
@@ -27,7 +28,7 @@ export class ChartTestComponent implements OnInit {
   @Input() refresh: Observable<void>;
 
   constructor(private service: ReturnsJsonArrayService) { }
-
+  
   ngOnInit() {
     setTimeout(() => {
       this.createChart();
@@ -37,7 +38,8 @@ export class ChartTestComponent implements OnInit {
       this.liste_cam_wifi_stations();
       this.tab_ble();
       this.tab_rtl();
-      setTimeout(this.calculPourcentage, 5000)
+      this.calculPourcentage();
+      setTimeout(this.createChart, 1000)
     });
   }
 
@@ -47,78 +49,100 @@ export class ChartTestComponent implements OnInit {
 
   calculPourcentage() {
     let total = 0;
-    console.log("pourcent");
-    this.content_tab_ble.forEach(element => {
-      this.content_cam_ble++;
-    });
-    this.content_tab_wifi.forEach(element => {
-      this.content_cam_wifi++;
-    });
-    console.log("apres");
-    this.content_tab_rtl.forEach(element => {
-      this.content_cam_rtl++;
-    });
-    this.content_tab_stations.forEach(element => {
-      this.content_cam_stations++;
-    });
-    total += this.content_cam_wifi;
-    total += this.content_cam_stations;
-    total += this.content_cam_ble;
-    total += this.content_cam_rtl;
-    console.log(this.content_tab_ble);
-    console.log(this.content_tab_wifi);
-    console.log(this.content_tab_stations);
-    console.log(this.content_tab_rtl);
-    this.content_cam_wifi = this.content_cam_wifi / total;
-    this.content_cam_stations = this.content_cam_stations / total;
-    this.content_cam_ble = this.content_cam_ble / total;
-    this.content_cam_rtl = this.content_cam_rtl / total;
-    this.createChart();
+  //   console.log("pourcent");
+  //   console.log(this.content_tab_ble);
+  //   console.log("fin");
+  // if (this.content_tab_ble) {
+  //   this.content_tab_ble.forEach(element => {
+  //     this.content_cam_ble++;
+  //   });
+  // }
+
+  // if (this.content_tab_wifi2) {
+  //   this.content_tab_wifi2.forEach(element => {
+  //     this.content_cam_wifi++;
+  //   });
+  // }
+
+  
+  // if (this.content_tab_rtl) {
+  //   this.content_tab_rtl.forEach(element => {
+  //     this.content_cam_rtl++;
+  //   });
+  // }
+
+  // if (this.content_tab_stations) {
+  //   this.content_tab_stations.forEach(element => {
+  //     this.content_cam_stations++;
+  //   });
+  // }
+  
+  //   total += this.content_cam_wifi;
+  //   total += this.content_cam_stations;
+  //   total += this.content_cam_ble;
+  //   total += this.content_cam_rtl;
+  
+  //    console.log(this.content_tab_wifi2);
+  // //   console.log(this.content_tab_wifi2);
+  //    console.log(this.content_tab_stations);
+  //    console.log(this.content_tab_rtl);
+  //   total = this.content_tab_ble + this.content_tab_rtl + this.content_tab_stations + this.content_tab_wifi2;
+  //   console.log(total);
+  //   this.content_cam_wifi = this.content_cam_wifi / total;
+  //   this.content_cam_stations = this.content_cam_stations / total;
+  //   this.content_cam_ble = this.content_cam_ble / total;
+  //   this.content_cam_rtl = this.content_cam_rtl / total;
+    //this.createChart();
+    console.log(this.content_tab_wifi2); //Affiche une erreur Undifined
+    //total = this.content_tab_wifi2 element.cam_ble qui vaut 67 + element.wifi qui vaut 78 par ex
   }
 
 
   tab_wifi() {
     // console.log('http://'  + this.ip + '/project/dossier_json/req_tab_wifi2.json');
-    this.service.getRequest('http://' + this.ip + '/project/dossier_json/req_tab_wifi2.json')
+    this.service.getRequest('http://' + this.ip + '/project/dossier_json/req_cam1.json')
       .subscribe(
         (data) => {
-          this.content_tab_wifi = data.json();
-          
-          this.dateFilter();
+          this.content_tab_wifi2 = Object.assign({}, data.json());
+          console.log("coucou");
+          console.log(this.content_tab_wifi2);
+          console.log("finfin");
+         // this.dateFilter();
         },
         (err) => { console.log(err) }
       );
+      this.calculPourcentage();
   }
   liste_cam_wifi_stations() {
     // console.log('http://'  + this.ip + '/project/dossier_json/req_cam2.json');
     this.service.getRequest('http://' + this.ip + '/project/dossier_json/req_cam2.json')
       .subscribe(
         (data) => {
-          this.content_cam_stations = data.json()["COUNT(*)"];
-          this.isInit = true;
+          this.content_cam_stations = data.json();
+         // this.isInit = true;
         },
         (err) => { console.log(err) }
       );
   }
   tab_rtl(){
     // console.log('http://'  + this.ip + '/project/dossier_json/req_tab_rtl.json');
-    this.service.getRequest('http://'  + this.ip + '/project/dossier_json/req_tab_rtl.json')
+    this.service.getRequest('http://'  + this.ip + '/project/dossier_json/req_cam3.json')
     .subscribe(
       (data) => {
         this.content_tab_rtl = data.json();
-        console.log(this.content_tab_rtl);
-        this.dateFilter();
+       //console.log(this.content_tab_rtl);
+       // this.dateFilter();
       },
       (err) => {console.log(err)}
     );
   }
   tab_ble(){
     // console.log('http://'  + this.ip + '/project/dossier_json/req_tab_ble1.json');
-    this.service.getRequest('http://'  + this.ip + '/project/dossier_json/req_tab_ble1.json')
+    this.service.getRequest('http://'  + this.ip + '/project/dossier_json/req_cam4.json')
     .subscribe(
       (data) => {
         this.content_tab_ble = data.json();
-        this.dateFilter();
+       // this.dateFilter();
       },
       (err) => {console.log(err)}
     );
@@ -128,13 +152,13 @@ export class ChartTestComponent implements OnInit {
     // var dateClick = new Date(this.date);
     // // console.log(dateClick);
 
-    // // console.log(this.content_tab_wifi);
-    // this.content_tab_wifi.filter((element) => {
+    // // console.log(this.content_tab_wifi2);
+    // this.content_tab_wifi2.filter((element) => {
     //   var dateElement = new Date(element.last_time_seen);
     //   // console.log(element.last_time_seen);
     //   return dateElement >= dateClick;
     // });
-    // // console.log(this.content_tab_wifi);
+    // // console.log(this.content_tab_wifi2);
   }
 
   createChart() {
